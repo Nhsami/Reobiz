d921235f38479cdd5ec45379714797608c0950c3
Sami <89170924+Nhsami@users.noreply.github.com>
2021-08-19 22:59:59 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+let menubar = document.querySelector("#open-menu")
+
+// menubar.addEventListenar('click',=>{
+console.log('working')
+// })
+
+ function menuFunc() {
+
+	console.log('working');
+	document.querySelector(".mainNavbar").classList.toggle("showMenu");
+
+	this.querySelector("ul li a span i").classList.toggle("fa-caret-down")
+	
+};
+
+
+
+
+
+
+
+
+
+//  const elementList =document.querySelectorAll("ul.menu li");
+
+//  elementList.forEach(singleLi => {
+      
+// 	singleLi.addEventListener("click",) 
+//         this.querySelector("ul").classList.toggle("showMenu")
+// 		this.querySelector("ul li a span i").classList.toggle("fa-caret-up")
+
+	
+
+
+// });
